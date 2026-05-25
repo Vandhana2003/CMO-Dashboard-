7313f55ccc94a401a72802bb3d76dccc17181a6a
Vandhana M <mukarjeevandhana2003@gmail.com>
2026-05-25 22:11:42 +0530

    Update settingsController.js

--- server/controllers/settingsController.js
+++ server/controllers/settingsController.js
@@ -1,6 +1,6 @@
 const multer = require('multer');         
 const path = require('path'); 
-const { query } = require('../config/db');
+const { query } = require('../config/db'); 
 const { parseExcel, mergeHeaders, autoMapColumns, applyMappings, validateMappings, generateMappedExcel } = require('../utils/excelParser');
 const { calcDashboardKPIs, calcB2BKPIs, calcB2CKPIs, SYSTEM_PARAMETERS, getParametersForType } = require('../utils/formulas');
 const { fetchExternalData } = require('../services/externalApiService'); 
